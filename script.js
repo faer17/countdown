7c08011e3a7625f2235f9b3cec32962b016101d6
faer17 <faerumidovna17@gmail.com>
2023-06-12 12:58:24 +0500

    fix

---
+++ script.js
@@ -0,0 +1,18 @@
+var day = document.querySelector(".day")
+var hour = document.querySelector(".hour")
+var minutes = document.querySelector(".minutes")
+var moment = document.querySelector(".moment")
+function countdownNewYear() {
+  var newYear = new Date("January 1, 2024 00:00:00")
+  var nowYear = new Date()
+  var count = newYear - nowYear
+  var days = Math.floor(count / (1000 * 60 * 60 * 24))
+  var hours = Math.floor((count % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  var min = Math.floor((count % (1000 * 60 * 60)) / (1000 * 60))
+  var seconds = Math.floor((count % (1000 * 60)) / 1000)
+  day.innerHTML = days + "   " + "<span>Days</span>"
+  hour.innerHTML = hours + "  " + "<span>Hours</span>"
+  minutes.innerHTML = min + "  " + "<span>Minutes</span>"
+  moment.innerHTML = seconds + "  " + "<span>Seconds</span>"
+}
+setInterval(countdownNewYear, 1000)
